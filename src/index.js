45ad7a412f4e79e635aa6a829e2eef7ea179d68d
muno17 <munozd17@gmail.com>
2023-07-31 15:18:19 -0400

    add event listeners to tabs

--- src/index.js
+++ src/index.js
@@ -3,9 +3,29 @@ import { loadHome } from './home.js';
 import { loadMenu } from './menu.js';
 import { loadContact } from './contact.js';
 
+// initialize the page
 loader();
 loadHome();
 
+const main = document.getElementById("mainArea");
+
+// event listeners change the tab that's being displayed
 const home = document.getElementById('homeTab');
+home.addEventListener('click', () => {
+    main.innerHTML = ''
+    loadHome();
+})
+
+
 const menu = document.getElementById('menuTab');
+menu.addEventListener('click', () => {
+    main.innerHTML = "";
+    loadMenu();
+})
+
+
 const contact = document.getElementById('contactTab');
+contact.addEventListener('click', () => {
+    main.innerHTML = "";
+    loadContact();
+})
